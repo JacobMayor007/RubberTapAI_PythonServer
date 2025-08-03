f9ed40e5083a3be9645bcf5f0342b6ce32668213
JacobMayor007 <jacobmary.tapere007@gmail.com>
2025-08-03 16:34:17 +0800

    commit

--- server.py
+++ server.py
@@ -167,5 +167,5 @@ def predict():
 
 if __name__ == '__main__':
     # Use PORT from environment for Render.com
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port, threaded=True)
