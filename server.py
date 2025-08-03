8479a4f5cddafa905aa15de0511a22692f7a9478
JacobMayor007 <jacobmary.tapere007@gmail.com>
2025-08-03 18:38:17 +0800

    python server commit

--- server.py
+++ server.py
@@ -88,5 +88,6 @@ def predict():
             "details": str(e)
         }), 500
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, threaded=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
